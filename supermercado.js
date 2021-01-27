60f122ed73698244e392a10d856219738ce57c95
Pedro <pedrodutra1r@gmail.com>
2021-01-26 23:22:32 -0300

    correçao

--- supermercado.js
+++ supermercado.js
@@ -2,26 +2,19 @@ var items= [];
 
 document.querySelector('input[type=submit]')
 .addEventListener('click',()=>{
-    var nomeProduto = document.querySelector('input[name=nome_produto]').value;
-    var precoProduto = document.querySelector('input[name=price]').value;
+    var nomeProduto = document.querySelector('input[name=nome_produto]');
+    var precoProduto = document.querySelector('input[name=price]');
 
     items.push({
         nome:nomeProduto.value,
         valor: precoProduto.value
     });
-
-
-    /*
-        <div class="lista-produto-single">
-            <h3>RedBull</h3>
-            <h3 class="price-produto"><span>R$20,00</span></h3>
-        </div>       
-    */
+       
     let listaProdutos = document.querySelector('.lista-produtos');
     let soma = 0;
     listaProdutos.innerHTML = "";
     items.map(function(val){
-        soma += parseFloat(val.valor);
+        soma += parseInt(val.valor);
 
         listaProdutos.innerHTML+=`
         <div class="lista-produto-single">
@@ -31,17 +24,17 @@ document.querySelector('input[type=submit]')
         `;
     });
     soma = soma.toFixed(2);
-    nomeProduto.value= "";
-    precoProduto = "";
+    nomeProduto.value = "";
+    precoProduto.value = "";
     let elementoSoma = document.querySelector(".soma-produto h1");
     elementoSoma.innerHTML = 'R$'+soma;
 
 });
 
 
-document.querySelector('button[neme=limpar]')
+document.querySelector('button[name=limpar]')
 .addEventListener('click',()=>{
     items = [];
     document.querySelector('.lista-produtos').innerHTML = "";
-    document.querySelector(".soma-produto h1").innerHTML = "R$0";
+    document.querySelector(".soma-produto h1").innerHTML = "Total: R$0";
 })
